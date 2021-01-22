aa23bb0dbc4f280d5dd67bcb7c1622696f48ec47
tuongvule <tuongvule@gmail.com>
2021-01-22 16:09:35 +0700

    Chapter 7: Signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "static_pages/help"
   scope "(:locale)", locale: /en|vi/ do
     resources :microposts
-    resources :users
+    resources :users, except: %i(new create)
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,4 @@
-
-
 ActiveRecord::Schema.define(version: 2021_01_26_074531) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -9,5 +6,4 @@ ActiveRecord::Schema.define(version: 2021_01_26_074531) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
   end
-
 end
